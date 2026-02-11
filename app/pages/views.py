b043bbd6b36e43fb3a4b6b9b9aba29147d454e17
Miku-27 <145240942+Miku-27@users.noreply.github.com>
2026-02-11 11:02:33 +0530

    Pass API key to get_book_info_service

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, render_template, redirect
+from flask import Blueprint, request, render_template, redirect,current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity,verify_jwt_in_request
 
 from app.services.library_service import get_user_books,get_book_info_service
@@ -21,7 +21,8 @@ def library_route():
 def book_detail_route(book_id):
 
     user_id = get_jwt_identity() 
-    book_result = get_book_info_service(book_id,user_id)
+    api_key=current_app.config["GOOGLE_API_KEY"]
+    book_result = get_book_info_service(api_key,book_id,user_id)
     return render_template("book-detail.html",book = book_result.get("data").get("book"),book_existence = book_result.get("data").get("book_existence"))
 
 @view.route("/")
